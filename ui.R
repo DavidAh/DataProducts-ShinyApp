621ee2f553b6dbbfd5fb88d153e1545b1bf43a71
DavidAh <ahrensdc@gmail.com>
2015-04-16 20:24:57 -0700

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,35 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("MyApp"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Enter your height and weight to compute your Body Mass Index."),
+      br(),
+      helpText("Enter your height in feet and inches"),
+      numericInput("feet", 
+                   label = h3("height in feet"), 
+                   value = 5),
+      numericInput("inches", 
+                   label = h3("height in inches"), 
+                   value = 0),
+      
+      helpText("Enter your weight in pounds (lbs)"),
+      sliderInput("lbs", 
+                  label = "Weight in pounds",
+                  min = 75, max = 400, value = 100),
+      
+    
+      submitButton("Compute BMI", ""),
+      br(),
+      br()
+      
+    ),
+    mainPanel(
+      textOutput("text1"),
+      textOutput("text2"),
+      textOutput("BMI")
+      )
+  )
+))
