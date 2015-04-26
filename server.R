315184ed8777be2fa53dc4111db5166846d3556d
DavidAh <ahrensdc@gmail.com>
2015-04-26 12:57:47 -0700

    Update server.R
    Added call to renderTable to draw the BMI table.

--- server.R
+++ server.R
@@ -4,6 +4,10 @@ library(quantmod)
 
 BMIcalc  <- function(feet, inches, w) (w /(feet * 12 + inches)^2 ) * 703
 heightCalc <- function(feet, inches) feet * 12 + inches
+bmi_vec <- c("<18.5", "18.5 - 24.9", "25.0 - 29.9", "> 30.0")
+w_vec <- c("Underweight", "Normal", "Overweight", "Obese")
+bmi_table <- cbind(bmi_vec, w_vec)
+colnames(bmi_table) <- c("BMI", "Weight Status")
 
 shinyServer(function(input, output) {
   
@@ -14,9 +18,11 @@ shinyServer(function(input, output) {
   output$text2 <- renderText({ 
     paste("Your weight in pounds (lbs)", input$lbs)
   })
-  
+
  output$BMI <- renderText({ 
-    paste("Your Body Mass Index (BMI)", BMIcalc(input$feet, input$inches, input$lbs))
+    paste("Your Body Mass Index (BMI)  is", BMIcalc(input$feet, input$inches, input$lbs))
   })
+ 
+ output$BMI_table <- renderTable({bmi_table})
   
  })
